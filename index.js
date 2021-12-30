0d718ee095887f168443ac7a9701a5454bffd2d7
buihainghiaaa <95730026+buihainghiaaa@users.noreply.github.com>
2021-12-30 14:35:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 $("#login-button").click(function (event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    if (userName == "minhphuong" && pwd == "123") {
+    if (userName == "" && pwd == "123") {
       event.preventDefault();
       $("form").fadeOut(500);
       $(".wrapper").addClass("form-success");
